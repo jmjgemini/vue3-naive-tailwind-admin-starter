468f6e212a73607e28a1c99b32f85d870ba04c35
JimJay <jmjgeminix@gmail.com>
2022-07-10 00:21:28 +0800

    A little try for dark mode toggle

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
-import { NConfigProvider, NButton } from "naive-ui";
+import { NConfigProvider, NButton, NGlobalStyle } from "naive-ui";
 import { zhCN, dateZhCN } from "naive-ui";
+import { lightTheme, darkTheme } from "naive-ui";
+import { ref } from "vue";
+const theme = ref(lightTheme);
+function changeTheme() {
+  if (theme.value.name == "light") {
+    theme.value = darkTheme;
+  } else {
+    theme.value = lightTheme;
+  }
+}
 </script>
 
 <template>
-  <n-config-provider :locale="zhCN" :date-locale="dateZhCN">
-    <n-button type="primary">hello</n-button>
+  <n-config-provider :theme="theme" :locale="zhCN" :date-locale="dateZhCN">
+    <n-button type="primary" @click="changeTheme">ChangeTheme</n-button>
+    <n-global-style />
   </n-config-provider>
 </template>
 
