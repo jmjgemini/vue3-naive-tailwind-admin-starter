627340dd9e85ec2becaacdf2bbfb220a16be05a6
JimJay <jmjgeminix@gmail.com>
2022-07-10 19:12:55 +0800

    try the basic layout

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,7 @@
 import { NConfigProvider, NButton, NGlobalStyle } from "naive-ui";
 import { zhCN, dateZhCN } from "naive-ui";
 import { lightTheme, darkTheme } from "naive-ui";
+import { NLayout, NLayoutHeader, NLayoutSider, NLayoutContent } from "naive-ui";
 import { ref } from "vue";
 const theme = ref(lightTheme);
 function changeTheme() {
@@ -15,18 +16,28 @@ function changeTheme() {
 
 <template>
   <n-config-provider :theme="theme" :locale="zhCN" :date-locale="dateZhCN">
-    <n-button type="primary" @click="changeTheme">ChangeTheme</n-button>
+    <n-layout position="absolute">
+      <n-layout-header style="height: 64px; padding: 24px" bordered>
+        <n-button type="primary" @click="changeTheme">ChangeTheme</n-button>
+      </n-layout-header>
+      <n-layout has-sider position="absolute" bordered style="top: 64px">
+        <n-layout-sider content-style="padding: 24px;" bordered
+          >sider</n-layout-sider
+        >
+        <n-layout-content content-style="padding: 24px;" bordered>
+          content
+        </n-layout-content>
+      </n-layout>
+    </n-layout>
+
     <n-global-style />
   </n-config-provider>
 </template>
 
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,8 @@ import pinia from "./store";
 setupTailwind();
 createApp(App).use(pinia).mount("#app");
 
-// 解决tailwind和naive ui冲突的问题
-// see https://www.naiveui.com/zh-CN/light/docs/style-conflict
+// 解决tailwind和naive ui样式冲突的问题
+// check https://www.naiveui.com/zh-CN/light/docs/style-conflict
 function setupTailwind() {
   const meta = document.createElement("meta");
   meta.name = "naive-ui-style";
