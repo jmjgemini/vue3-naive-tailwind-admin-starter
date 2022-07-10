a733488591de8039854a99384788f716a9cc091a
JimJay <jmjgeminix@gmail.com>
2022-07-11 00:49:50 +0800

    good try

--- src/layouts/main-layout.vue
+++ src/layouts/main-layout.vue
@@ -6,7 +6,7 @@ import {
   NLayoutContent,
   NIcon
 } from "naive-ui";
-import { NMenu, NButton } from "naive-ui";
+import { NMenu, NButton, NDropdown, NSpace } from "naive-ui";
 import type { MenuOption } from "naive-ui";
 import { DotCircle, Book, Bookmark } from "@vicons/fa";
 import { h, Component } from "vue";
@@ -142,12 +142,36 @@ const menuOptions: MenuOption[] = [
     icon: renderIcon(DotCircle)
   }
 ];
+
+const dropDownOptions = [
+  {
+    label: "个人设置",
+    key: "personal-setting"
+  },
+  {
+    label: "退出登录",
+    key: "logout"
+  }
+];
 </script>
 
 <template>
   <n-layout position="absolute">
-    <n-layout-header style="height: 64px; padding: 24px" bordered>
-      <span class="text-green-600 text-xl">Naive Admin Pro</span>
+    <n-layout-header style="height: 64px" bordered>
+      <div class="flex flex-row items-center justify-between h-full px-4">
+        <div class="text-xl">Naive Admin Pro</div>
+        <n-space>
+          <n-button size="small" strong secondary type="success">
+            切换深色模式
+          </n-button>
+          <n-button size="small" strong secondary type="success">
+            偏好设置
+          </n-button>
+          <n-dropdown :options="dropDownOptions">
+            <div class="cursor-pointer">你好,管理员</div>
+          </n-dropdown>
+        </n-space>
+      </div>
     </n-layout-header>
     <n-layout has-sider position="absolute" bordered style="top: 64px">
       <n-layout-sider
@@ -166,7 +190,7 @@ const menuOptions: MenuOption[] = [
           :collapsed-icon-size="20"
         />
       </n-layout-sider>
-      <n-layout-content content-style="padding: 24px;" bordered>
+      <n-layout-content bordered>
         <router-view />
       </n-layout-content>
     </n-layout>
